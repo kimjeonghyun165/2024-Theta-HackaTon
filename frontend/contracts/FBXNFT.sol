645238be1efdf9535f354cb791760ad04939f545
JeongHyun <kdsocks5@naver.com>
2024-07-13 20:07:56 +0900

    feat: contract code

--- frontend/contracts/FBXNFT.sol
+++ frontend/contracts/FBXNFT.sol
@@ -10,13 +10,32 @@ contract FBXNFT is ERC721URIStorage {
 
     address admin;
 
-    event mintedNFT(
+    struct NFT {
+        uint256 tokenId;
+        address payable owner;
+        uint256 price;
+        bool forSale;
+    }
+
+    mapping(uint256 => NFT) public nfts;
+
+    event MintedNFT(
         address indexed owner,
         uint256 indexed tokenId,
         string tokenUrl
     );
 
-    constructor() ERC721("Anvil3dNFT", "ANV3D") {}
+    event ListedNFT(uint256 indexed tokenId, uint256 price, bool forSale);
+
+    event PurchasedNFT(
+        uint256 indexed tokenId,
+        address indexed newOwner,
+        uint256 price
+    );
+
+    constructor() ERC721("Anvil3dNFT", "ANV3D") {
+        admin = msg.sender;
+    }
 
     function createNFT(string memory tokenUrl) public returns (uint256) {
         _tokenIdCounter.increment();
@@ -25,8 +44,50 @@ contract FBXNFT is ERC721URIStorage {
         _safeMint(msg.sender, newTokenId);
         _setTokenURI(newTokenId, tokenUrl);
 
-        emit mintedNFT(msg.sender, newTokenId, tokenUrl);
+        nfts[newTokenId] = NFT(newTokenId, payable(msg.sender), 0, false);
+
+        emit MintedNFT(msg.sender, newTokenId, tokenUrl);
 
         return newTokenId;
+    }
+
+    function listNFT(uint256 tokenId, uint256 price) public {
+        require(
+            ownerOf(tokenId) == msg.sender,
+            "Only the owner can list this NFT"
+        );
+        require(price > 0, "Price must be greater than zero");
+
+        nfts[tokenId].price = price;
+        nfts[tokenId].forSale = true;
+
+        emit ListedNFT(tokenId, price, true);
+    }
+
+    function buyNFT(uint256 tokenId) public payable {
+        NFT memory nft = nfts[tokenId];
+        require(nft.forSale, "This NFT is not for sale");
+        require(msg.value == nft.price, "Please submit the asking price");
+
+        address payable seller = nft.owner;
+        _transfer(seller, msg.sender, tokenId);
+
+        seller.transfer(msg.value);
+
+        nfts[tokenId].owner = payable(msg.sender);
+        nfts[tokenId].forSale = false;
+
+        emit PurchasedNFT(tokenId, msg.sender, nft.price);
+    }
+
+    function cancelSale(uint256 tokenId) public {
+        require(
+            ownerOf(tokenId) == msg.sender,
+            "Only the owner can cancel the sale"
+        );
+
+        nfts[tokenId].forSale = false;
+
+        emit ListedNFT(tokenId, nfts[tokenId].price, false);
     }
 }
