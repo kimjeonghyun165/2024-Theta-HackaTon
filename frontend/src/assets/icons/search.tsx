8955fa534ccaecd515c6b0d93b2545c681d55202
BaDook2 <wkdrudqls114@gmail.com>
2024-07-13 14:34:44 +0900

    add: more design. final scrollbar

---
+++ frontend/src/assets/icons/search.tsx
@@ -0,0 +1,12 @@
+
+const Search = () => {
+  return (
+    <svg width="30" height="27" viewBox="0 0 30 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect y="1.36678" width="17.3832" height="5.14118" transform="matrix(0.730054 0.683389 -0.730054 0.683389 16.2613 9.30627)" stroke="white" stroke-width="2" />
+      <path d="M22.4928 10.8455C22.4928 16.0684 17.9443 20.4409 12.1568 20.4409C6.36925 20.4409 1.82071 16.0684 1.82071 10.8455C1.82071 5.62253 6.36925 1.25 12.1568 1.25C17.9443 1.25 22.4928 5.62253 22.4928 10.8455Z" fill="#26272A" stroke="white" stroke-width="2.5" />
+    </svg>
+
+  )
+}
+
+export default Search
--- frontend/src/pages/myPage/myPage.tsx
+++ frontend/src/pages/myPage/myPage.tsx
@@ -6,6 +6,8 @@ import { Ex1 } from "../../assets/imgSelect";
 import { Check } from "../../assets/icons";
 import ModelBox from "../../components/myPage/modelBox";
 import "../../components/myPage/scrollbarStyle.css"
+import Edit from "../../assets/icons/edit";
+import Search from "../../assets/icons/search";
 const MyPage: React.FC = () => {
   const [isChecked, setIsChecked] = useState(false);
   return (
@@ -13,11 +15,16 @@ const MyPage: React.FC = () => {
       <section className="mx-auto w-4/5 bg-[#D0D0D0]/[.07] rounded-3xl flex flex-col">
         <div className="w-full h-full pt-10 px-14 flex flex-col gap-20">
           <section className="flex gap-14 h-[666px]">
-            <div className="flex-1 border-2"><Ex1 /></div>
+            <div className="flex-1 border-2 rounded-[30px] relative" style={{ background: 'linear-gradient(135deg, rgba(36, 72, 98, 0.09) 0%, rgba(255, 255, 255, 0.27) 55.1%, rgba(255, 255, 255, 0.27) 63.6%, rgba(36, 72, 98, 0.3) 100%)' }}><Ex1 /><button className="w-[33px] h-[33px] absolute top-3 right-6"><Edit /></button></div>
+
             <div className="flex-1 border-2 flex flex-col justify-between">
               <div className="flex flex-col gap-3">
-                <h1 className="text-2xl">Platinum BlackSmith</h1>
-                <h2 className="text-5xl">Macallan Craigellachie</h2>
+                <h1 className="text-2xl flex items-center gap-2">
+                  <div className="w-[27px] h-[27px] rounded-md" style={{ borderRadius: '10px 0px 10px 13.5px', background: 'linear-gradient(135deg, rgba(36, 72, 98, 0.3) 0%, rgba(255, 255, 255, 0.9) 55.1%, rgba(255, 255, 255, 0.9) 63.6%, #244862 100%)' }} />
+                  Platinum BlackSmith</h1>
+                <h2 className="text-5xl relative">Macallan Craigellachie
+                  <button className="absolute right-0 bottom-[50%]"><Edit /></button>
+                </h2>
               </div>
               <div className="flex flex-col gap-4">
                 <h3 className="text-2xl">Achievements</h3>
@@ -56,13 +63,19 @@ const MyPage: React.FC = () => {
             </div>
           </section>
           <section>
-            <div className="flex text-2xl">
+            <div className="flex text-2xl gap-10">
               <button className="w-[51px] h-[51px] rounded-[5px] bg-[#777]/[0.2] flex justify-center items-center" onClick={() => setIsChecked((prev) => !prev)}>
                 {isChecked ? <Check /> : null}
               </button>
-              <input type="text" className="w-[704px] rounded-[30px] bg-[#777]/[0.2] placeholder:text-white placeholder:text-2xl" placeholder="Search" />
-              <button>
-                Recent
+              <div className="w-[704px] rounded-[30px] bg-[#777]/[0.2] placeholder:text-white placeholder:text-2xl flex items-center px-5 gap-3">
+                <div className="w-[29px] h-[27px]">
+                  <Search />
+                </div>
+                <span className="text-white text-2xl">Search:</span>
+                <input type="text" className="bg-transparent outline-none text-white" />
+              </div>
+              <button className="right-20 text-white ml-12">
+                Recent ▼
               </button>
             </div>
             <div className="max-h-[500px] overflow-y-scroll grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 4xl:grid-cols-5 gap-4 mt-10 myPage-scrollbar">
