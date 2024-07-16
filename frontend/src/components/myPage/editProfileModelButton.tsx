0229cbfac9908f1ab0ce7be784bb8945b31958c3
unknown <wkdrudqls114@gmail.com>
2024-07-17 01:02:38 +0900

    my page

--- frontend/src/components/myPage/editProfileModelButton.tsx
+++ frontend/src/components/myPage/editProfileModelButton.tsx
@@ -25,27 +25,28 @@ const EditMyModelButton = () => {
       <button className="w-[33px] h-[33px] absolute top-3 right-6" onClick={openModal}><Edit /></button>
       <dialog
         ref={modalRef}
-        className="backdrop:backdrop-blur-md rounded-[87px] py-10 px-28" style={{
+        className="backdrop:bg-[#030408]  rounded-[87px] py-12 px-28" 
+        onClick={(e) => { if (e.target === modalRef.current) closeModal() }}
+        style={{
           background:
             "linear-gradient(180deg, rgba(208, 208, 208, 0.25) 0%, rgba(106, 106, 106, 0.5) 100%)",
         }}
-        onClick={(e) => { if (e.target === modalRef.current) closeModal() }}
       >
-        <section className=''>
-          <h2 className='text-3xl font-bold'>PROFILE ASSET SELECTION</h2>
-          <div className="flex gap-10 text-2xl">
-            <div className="w-[704px] rounded-[30px] bg-[#777]/[0.2] placeholder:text-white placeholder:text-2xl flex items-center px-5 gap-3">
+        <section>
+          <h2 className='text-3xl font-bold pl-9 text-second'>PROFILE ASSET SELECTION</h2>
+          <div className="flex justify-between px-5 mt-10 text-2xl">
+            <div className="w-2/3 rounded-[30px] bg-[#777]/[0.2] placeholder:text-white placeholder:text-2xl flex items-center px-5 gap-3 py-3">
               <div className="w-[29px] h-[27px]">
                 <Search />
               </div>
               <span className="text-2xl text-white">Search:</span>
               <input type="text" className="text-white bg-transparent outline-none" />
             </div>
-            <button className="ml-12 text-white right-20">
+            <button className="mr-10 text-white">
               Recent ▼
             </button>
           </div>
-          <div className="max-h-[500px] overflow-y-scroll grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 4xl:grid-cols-5 gap-4 mt-10 myPage-scrollba">
+          <div className="max-w-[1575px] max-h-[500px] overflow-y-scroll grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 4xl:grid-cols-5 gap-14 mt-10 myPage-scrollbar pr-7">
             {new Array(12).fill(0).map((_, index) => {
               return (
                 <button
