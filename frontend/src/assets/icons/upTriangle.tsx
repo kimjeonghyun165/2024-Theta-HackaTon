d60850a956da3ff8cba8252b59c19f3e062ea453
장경빈 <wkdrudqls114@gmail.com>
2024-07-27 06:29:12 +0000

    frontend/src/assets/icons/upTriangle.tsx edited online with Bitbucket

--- frontend/src/assets/icons/upTriangle.tsx
+++
@@ -1,9 +0,0 @@
-const UpTriangle = () => {
-  return (
-    <svg width="20" height="17" viewBox="0 0 20 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M10.0093 17L0.337281 0.272001H19.6573L10.0093 17Z" fill="white" />
-    </svg>
-  )
-}
-
-export default UpTriangle
