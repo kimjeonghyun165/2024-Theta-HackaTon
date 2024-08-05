02ee0b825d7c8ca8ba59ad3726a4a3ebed13be19
JeongHyun <kdsocks5@naver.com>
2024-08-05 16:55:04 +0900

    add:user update api

--- frontend/src/components/common/layout/myPage/Layout.tsx
+++ frontend/src/components/common/layout/myPage/Layout.tsx
@@ -1,5 +1,5 @@
 import { Header } from "../Header";
-import "./mypage.css";
+
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="flex flex-col min-h-screen mx-auto min-w-[1024px] max-w-[2400px]">
